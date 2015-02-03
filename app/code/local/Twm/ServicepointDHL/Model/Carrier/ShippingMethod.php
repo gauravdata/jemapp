af5e026adac660dd5e6e40e0c092ed4bdee316ff
Rob Versluis <rob@thewebmen.com>
2015-02-03 13:08:56 +0100

    servicepoint

--- app/code/local/Twm/ServicepointDHL/Model/Carrier/ShippingMethod.php
+++ app/code/local/Twm/ServicepointDHL/Model/Carrier/ShippingMethod.php
@@ -36,13 +36,13 @@ class Twm_ServicepointDHL_Model_Carrier_ShippingMethod extends Mage_Shipping_Mod
         $cached = Mage::app()->getCache();
         if (($result = $cached->load($key)) !== false) {
             $result = Zend_Json::decode($result);
-            $result = array_slice($result['data']['items'], 0, 6);
+            $result = array_slice($result['data']['items'], 0, 3);
             return $result;
         }
 
         $query = !empty($postcode) ? $postcode : $city;
 
-        $uri = 'https://dhlforyounl-dhlforyounl-service-point-locator.p.mashape.com/datamoduleAPI.jsp?action=public.splist&ot=n&v=2&s=' . urlencode($query);
+        $uri = 'https://dhlforyounl-dhlforyounl-service-point-locator.p.mashape.com/datamoduleAPI.jsp?action=public.splist&ot=y&v=2&s=' . urlencode($query);
         $client = new Zend_Http_Client($uri);
         $client->setHeaders(array(
             'X-Mashape-Key' => '2mkhycZJq1msh6dAfgbllXxrSr5Wp1rotFHjsnEknupB8oHZcD',
@@ -55,7 +55,7 @@ class Twm_ServicepointDHL_Model_Carrier_ShippingMethod extends Mage_Shipping_Mod
             $cached->save($response->getBody(), $key, array("dhl"), 30 * 24 * 3600);
             $result = Zend_Json::decode($response->getBody());
 
-            $result = array_slice($result['data']['items'], 0, 6);
+            $result = array_slice($result['data']['items'], 0, 3);
             return $result;
         }
         return array();
@@ -90,11 +90,16 @@ class Twm_ServicepointDHL_Model_Carrier_ShippingMethod extends Mage_Shipping_Mod
         foreach ($this->getDHLAddresses($postcode, $city) as $carrier) {
             $method = Mage::getModel('shipping/rate_result_method');
 
+            $_address = '<br/>';
+            $_address .= $carrier['add'] . '<br/>';
+            $_address .= $carrier['zip'] .' ' . $carrier['city'] . '<br/>';
+            //$_address .= '<a href="#" data-toggle="popover" data-placement="top" data-html="true" data-trigger="focus" title="Openingstijden" data-content="'.$carrier['d_opening'].'">Openingstijden</a>';
+
             $method->setCarrier($this->_code);
             $method->setCarrierTitle($carrierTitle);
 
             $method->setMethod($carrier['psid']);
-            $method->setMethodTitle($carrier['name']);
+            $method->setMethodTitle($carrier['name'] . $_address);
 
             $method->setCost($price);
             $method->setPrice($price);
