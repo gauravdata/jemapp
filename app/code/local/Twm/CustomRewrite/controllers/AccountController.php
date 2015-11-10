f5dd17055d2249a35cf2509e80d78e6cffcdf38d
Mathijs Futselaar <mathijs@thewebmen.com>
2015-11-10 11:38:11 +0100

    Disabled password validation

---
+++ app/code/local/Twm/CustomRewrite/controllers/AccountController.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mathijs
+ * Date: 10-11-15
+ * Time: 11:27
+ */
+class Twm_CustomRewrite_AccountController extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * Rewrite so users can change password without providing their current password
+     *
+     * @return $this|Mage_Core_Controller_Varien_Action|void
+     */
+
+
+    public function editPostAction()
+    {
+        if (!$this->_validateFormKey()) {
+            return $this->_redirect('*/*/edit');
+        }
+
+        if ($this->getRequest()->isPost()) {
+            /** @var $customer Mage_Customer_Model_Customer */
+            $customer = $this->_getSession()->getCustomer();
+
+            /** @var $customerForm Mage_Customer_Model_Form */
+            $customerForm = $this->_getModel('customer/form');
+            $customerForm->setFormCode('customer_account_edit')
+                ->setEntity($customer);
+
+            $customerData = $customerForm->extractData($this->getRequest());
+
+            $errors = array();
+            $customerErrors = $customerForm->validateData($customerData);
+            if ($customerErrors !== true) {
+                $errors = array_merge($customerErrors, $errors);
+            } else {
+                $customerForm->compactData($customerData);
+                $errors = array();
+
+                // If password change was requested then add it to common validation scheme
+                if ($this->getRequest()->getParam('change_password')) {
+
+                    $newPass    = $this->getRequest()->getPost('password');
+                    $confPass   = $this->getRequest()->getPost('confirmation');
+
+                    if (strlen($newPass)) {
+                        /**
+                         * Set entered password and its confirmation - they
+                         * will be validated later to match each other and be of right length
+                         */
+                        $customer->setPassword($newPass);
+                        $customer->setConfirmation($confPass);
+                    }
+                    else
+                    {
+                        $errors[] = $this->__('New password field cannot be empty.');
+                    }
+                }
+
+                // Validate account and compose list of errors if any
+                $customerErrors = $customer->validate();
+                if (is_array($customerErrors)) {
+                    $errors = array_merge($errors, $customerErrors);
+                }
+            }
+
+            if (!empty($errors)) {
+                $this->_getSession()->setCustomerFormData($this->getRequest()->getPost());
+                foreach ($errors as $message) {
+                    $this->_getSession()->addError($message);
+                }
+                $this->_redirect('*/*/edit');
+                return $this;
+            }
+
+            try {
+                $customer->setConfirmation(null);
+                $customer->save();
+                $this->_getSession()->setCustomer($customer)
+                    ->addSuccess($this->__('The account information has been saved.'));
+
+                $this->_redirect('customer/account');
+                return;
+            } catch (Mage_Core_Exception $e) {
+                $this->_getSession()->setCustomerFormData($this->getRequest()->getPost())
+                    ->addError($e->getMessage());
+            } catch (Exception $e) {
+                $this->_getSession()->setCustomerFormData($this->getRequest()->getPost())
+                    ->addException($e, $this->__('Cannot save the customer.'));
+            }
+        }
+
+        $this->_redirect('*/*/edit');
+    }
+}
