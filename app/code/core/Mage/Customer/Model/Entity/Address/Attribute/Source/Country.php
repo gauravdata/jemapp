104029897521536ab244e3d1fdc480e871c8d074
Mathijs Futselaar <mathijs@thewebmen.com>
2015-11-20 11:32:09 +0100

    Installation Kiyoh update/upgrade

--- app/code/core/Mage/Customer/Model/Entity/Address/Attribute/Source/Country.php
+++ app/code/core/Mage/Customer/Model/Entity/Address/Attribute/Source/Country.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Magento
+ * Magento Enterprise Edition
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
+ * This source file is subject to the Magento Enterprise Edition License
+ * that is bundled with this package in the file LICENSE_EE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
+ * http://www.magentocommerce.com/license/enterprise-edition
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@magentocommerce.com so we can send you a copy immediately.
@@ -20,8 +20,8 @@
  *
  * @category    Mage
  * @package     Mage_Customer
- * @copyright   Copyright (c) 2013 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://www.magentocommerce.com/license/enterprise-edition
  */
 
 
@@ -35,33 +35,4 @@
 class Mage_Customer_Model_Entity_Address_Attribute_Source_Country
     extends Mage_Customer_Model_Resource_Address_Attribute_Source_Country
 {
-    /**
-     * Factory instance
-     *
-     * @var Mage_Core_Model_Abstract
-     */
-    protected $_factory;
-
-    /**
-     * Constructor for Mage_Customer_Model_Entity_Address_Attribute_Source_Country
-     *
-     * @param array $args
-     */
-    public function __construct(array $args = array())
-    {
-        $this->_factory = !empty($args['factory']) ? $args['factory'] : Mage::getSingleton('core/factory');
-    }
-    /**
-     * Retrieve all options
-     *
-     * @return array
-     */
-    public function getAllOptions()
-    {
-        if (!$this->_options) {
-            $this->_options = $this->_factory->getResourceModel('directory/country_collection')
-                ->loadByStore($this->getAttribute()->getStoreId())->toOptionArray();
-        }
-        return $this->_options;
-    }
 }
