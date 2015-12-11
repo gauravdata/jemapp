74da589bb002beffeee1622e90e83e445de6103d
Rob Versluis <rob@thewebmen.com>
2015-12-11 11:39:23 +0100

    update onestepcheckout and disable email domain validation

--- app/code/local/Twm/MinOrderQty/Model/Observer.php
+++ app/code/local/Twm/MinOrderQty/Model/Observer.php
@@ -12,7 +12,6 @@ class Twm_MinOrderQty_Model_Observer {
 		return Mage::getSingleton('checkout/cart');
 	}
 
-
 	public function checkMinOrderQty($observer) {
 		$event = $observer->getEvent();
 		$controller = $event->getControllerAction();
