02a5f4875858e99cbf69b1d51979627a3367d3ca
Mathijs Futselaar <mathijs@thewebmen.com>
2016-01-22 10:12:19 +0100

    Upgrade to 1.9.2.3

--- app/code/core/Mage/Customer/Model/Customer/Api/V2.php
+++ app/code/core/Mage/Customer/Model/Customer/Api/V2.php
@@ -1,27 +1,27 @@
 <?php
 /**
- * Magento Enterprise Edition
+ * Magento
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Magento Enterprise Edition License
- * that is bundled with this package in the file LICENSE_EE.txt.
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://www.magentocommerce.com/license/enterprise-edition
+ * http://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
+ * to license@magento.com so we can send you a copy immediately.
  *
  * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade Magento to newer
  * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
+ * needs please refer to http://www.magento.com for more information.
  *
  * @category    Mage
  * @package     Mage_Customer
- * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://www.magentocommerce.com/license/enterprise-edition
+ * @copyright  Copyright (c) 2006-2016 X.commerce, Inc. and affiliates (http://www.magento.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
@@ -46,65 +46,5 @@ class Mage_Customer_Model_Customer_Api_V2 extends Mage_Customer_Model_Customer_A
             return parent::_prepareData($_data);
         }
         return array();
-    }
-
-    /**
-     * Retrieve cutomers data
-     *
-     * @param  array $filters
-     * @return array
-     */
-    public function items($filters)
-    {
-        $collection = Mage::getModel('customer/customer')->getCollection()
-            ->addAttributeToSelect('*');
-
-        $preparedFilters = array();
-        if (isset($filters->filter)) {
-            foreach ($filters->filter as $_filter) {
-                $preparedFilters[$_filter->key] = $_filter->value;
-            }
-        }
-        if (isset($filters->complex_filter)) {
-            foreach ($filters->complex_filter as $_filter) {
-                $_value = $_filter->value;
-                $preparedFilters[$_filter->key] = array(
-                    $_value->key => $_value->value
-                );
-            }
-        }
-
-        if (!empty($preparedFilters)) {
-            try {
-                foreach ($preparedFilters as $field => $value) {
-                    if (isset($this->_mapAttributes[$field])) {
-                        $field = $this->_mapAttributes[$field];
-                    }
-                    $collection->addFieldToFilter($field, $value);
-                }
-            } catch (Mage_Core_Exception $e) {
-                $this->_fault('filters_invalid', $e->getMessage());
-            }
-        }
-
-        $result = array();
-        foreach ($collection as $customer) {
-            $data = $customer->toArray();
-            $row  = array();
-
-            foreach ($this->_mapAttributes as $attributeAlias => $attributeCode) {
-                $row[$attributeAlias] = (isset($data[$attributeCode]) ? $data[$attributeCode] : null);
-            }
-
-            foreach ($this->getAllowedAttributes($customer) as $attributeCode => $attribute) {
-                if (isset($data[$attributeCode])) {
-                    $row[$attributeCode] = $data[$attributeCode];
-                }
-            }
-
-            $result[] = $row;
-        }
-
-        return $result;
     }
 }
