dc030b91e1f1fe509555958b3bae7ffb675e805b
Mathijs Futselaar <mathijs@thewebmen.com>
2017-11-23 13:06:48 +0100

    Make rewards unavailable when the grandtotal doesnt match or exceed the configuration setting

--- app/code/local/Twm/ExtendAwPoints/Block/Points/Checkout/Onepage/Payment/Methods.php
+++ app/code/local/Twm/ExtendAwPoints/Block/Points/Checkout/Onepage/Payment/Methods.php
@@ -11,4 +11,27 @@ class Twm_ExtendAwPoints_Block_Points_Checkout_Onepage_Payment_Methods extends A
     {
         return Mage::getModel('pointsandrewards/method_pointsandrewards');
     }
+
+    public function pointsSectionAvailable()
+    {
+        $quote = Mage::getModel('checkout/session')->getQuote();
+        $quoteData = $quote->getData();
+        $grandTotal = $quoteData['grand_total'];
+
+        $minimumOrder = trim(str_replace(',','.', Mage::getStoreConfig('points/general/minimum_points_amount_for_spend')));
+
+        if ($minimumOrder != '' && $grandTotal >= $minimumOrder)
+        {
+            return parent::pointsSectionAvailable();
+        }
+        elseif ($minimumOrder != '' && $grandTotal < $minimumOrder)
+        {
+            return false;
+        }
+        else
+            return true;
+
+
+
+    }
 }
