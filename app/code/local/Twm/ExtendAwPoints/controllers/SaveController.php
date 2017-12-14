c8df4ef94ee417b4818ed7165f7ed3bc8e2200f4
Mathijs Futselaar <mathijs@thewebmen.com>
2017-12-14 16:09:08 +0100

    Don't unsubscribe

--- app/code/local/Twm/ExtendAwPoints/controllers/SaveController.php
+++ app/code/local/Twm/ExtendAwPoints/controllers/SaveController.php
@@ -4,7 +4,7 @@ class Twm_ExtendAwPoints_SaveController extends Mage_Core_Controller_Front_Actio
 {
     public function indexAction()
     {
-        Mage::helper('pointsandrewards')->toggleAllFlags((bool)(int)$this->getRequest()->getPost('club_jma'));
+        Mage::helper('pointsandrewards')->toggleAllFlags(true);
 
         return $this->_redirectReferer();
     }
