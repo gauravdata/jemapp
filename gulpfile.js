d15516540f750a07495e8adf7862b82251dd642f
Steven Lantinga <steven@thewebmen.com>
2018-02-01 15:02:55 +0100

    Grouped product

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 
 var paths = {
@@ -18,10 +17,8 @@ var paths = {
 gulp.task('styles', function () {
     gulp.src(paths.scss.src)
         .pipe(plumber())
-        .pipe(sourcemaps.init())
         .pipe(sass({includePaths: paths.scss.includes}))
         .pipe(minifyCSS({compatibility: 'ie8'}))
-        .pipe(sourcemaps.write())
         .pipe(gulp.dest(paths.scss.dest));
 });
 
