f196ddd58ace2b1afe9ee30c2b7b3613667c09a5
Marc van Bottenburg <marc@wehaveaplan.nl>
2019-04-01 17:35:13 +0200

    Afleveradres hetzelfde als de afhaallocatie als deze is gekozen.
    Dit is nodig voor Dealer4dealer, deze kent namelijk geen 3de adres.

---
+++ app/code/community/WHAP/Transsmart/Helper/Pickupaddress.php
@@ -0,0 +1,42 @@
+<?php
+class WHAP_Transsmart_Helper_Pickupaddress extends Transsmart_Shipping_Helper_Pickupaddress
+{
+	/**
+	 * @param Mage_Sales_Model_Quote $quote
+	 * @param Mage_Sales_Model_Quote_Address $pickupAddress
+	 * @return $this
+	 */
+	public function setQuotePickupAddress($quote, $pickupAddress)
+	{
+		$currentPickupAddress = null;
+		foreach ($quote->getAddressesCollection() as $_address) {
+			if (!$_address->isDeleted() && $_address->getAddressType() == 'transsmart_pickup') {
+				$currentPickupAddress = $_address;
+				break;
+			}
+		}
+
+		if (!empty($currentPickupAddress)) {
+			$currentPickupAddress->addData($pickupAddress->getData());
+		}
+		else {
+			$quote->addAddress($pickupAddress->setAddressType('transsmart_pickup'));
+		}
+
+		/** @var Mage_Sales_Model_Quote_Address $shippingAddress */
+		$shippingAddress = $quote->getShippingAddress();
+		$shippingAddress->setPrefix($pickupAddress->getPrefix())
+		                ->setFirstname($pickupAddress->getFirstname())
+		                ->setMiddlename($pickupAddress->getMiddlename())
+		                ->setLastname($pickupAddress->getLastname())
+		                ->setSuffix($pickupAddress->getSuffix())
+		                ->setTelephone($pickupAddress->getTelephone())
+		                ->setCompany($pickupAddress->getCompany())
+		                ->setPostcode($pickupAddress->getPostcode())
+		                ->setCity($pickupAddress->getCity())
+		                ->setCountryId($pickupAddress->getCountryId())
+		                ->setStreetFull($pickupAddress->getStreetFull());
+
+		return $this;
+	}
+}
