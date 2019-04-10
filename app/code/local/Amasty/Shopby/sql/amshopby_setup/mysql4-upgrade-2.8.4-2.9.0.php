641223b3add206bc2bc09c75b4873d85533813e6
Marc van Bottenburg <marc@wehaveaplan.nl>
2019-04-10 12:06:51 +0200

    Revert "Update fix Amasty Layered navigation extension."
    This reverts commit a874c480a894cf5cc64037e928dabe9f65d1d686.

--- app/code/local/Amasty/Shopby/sql/amshopby_setup/mysql4-upgrade-2.8.4-2.9.0.php
+++ app/code/local/Amasty/Shopby/sql/amshopby_setup/mysql4-upgrade-2.8.4-2.9.0.php
@@ -16,6 +16,10 @@ if (!$this->getConnection()->tableColumnExists($table, 'disable_seo_url')) {
 if (!$this->getConnection()->tableColumnExists($table, 'sort_featured_first')) {
     $this->run("ALTER TABLE `{$table}` ADD `sort_featured_first` TINYINT(1) NOT NULL DEFAULT '0' AFTER `sort_by`;");
 }
+if (!$this->getConnection()->tableColumnExists($table, 'number_options_for_show_search')) {
+    $this->run("ALTER TABLE `{$table}` ADD `number_options_for_show_search` INT(10) NOT NULL DEFAULT '0' AFTER `show_search`;");
+}
+$this->run("ALTER TABLE `{$table}` CHANGE COLUMN `slider_decimal` `slider_decimal` DECIMAL(12,4) NOT NULL DEFAULT '1.00' AFTER `number_options_for_show_search`;");
 $table = $this->getTable('amshopby/value');
 $this->run("ALTER TABLE `{$table}` ADD INDEX(`is_featured`)");
 
--- app/code/local/Amasty/Shopby/sql/amshopby_setup/mysql4-upgrade-3.0.9-3.1.0.php
+++ app/code/local/Amasty/Shopby/sql/amshopby_setup/mysql4-upgrade-3.0.9-3.1.0.php
@@ -19,6 +19,8 @@ if (!$this->getConnection()->tableColumnExists($table, 'use_mapping')) {
 }
 
 $this->run("
+ALTER TABLE `{$table}` CHANGE `depend_on` `depend_on` TEXT NOT NULL;
+ALTER TABLE `{$table}` CHANGE `depend_on_attribute` `depend_on_attribute` TEXT NOT NULL;
 ALTER TABLE `{$table}` CHANGE `exclude_from` `exclude_from` TEXT NOT NULL;
 ALTER TABLE `{$table}` CHANGE `include_in` `include_in` TEXT NOT NULL;
 ");
